a4742257aba21c8cec16915c0c49ba3b0d105da3
PrincewillDev <princewillfidelis1@gmail.com>
2023-03-25 12:38:39 -0700

    Add header file, it includes the standard libraries and include guard

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Include files */
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* Prototype */
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
